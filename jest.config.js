bab4a68e7053972c55e249e4432164920d42e934
tclindner <tclindner@users.noreply.github.com>
2019-10-27 19:44:36 -0500

    Add config files

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js', '!src/main.js', '!src/runCommand.js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  restoreMocks: true,
+  resetMocks: true,
+  resetModules: true,
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/']
+};
